4e81485f2d07957041bfc6e8e6cf3eb89d106d41
Tvaughn22 <vaughnt22@students.ecu.edu>
2025-02-17 13:30:52 -0500

    Registration tab

---
+++ ASP/ASP/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- ASP/ASP/Program.cs
+++ ASP/ASP/Program.cs
@@ -1,12 +1,36 @@
 using ASP.Components;
+using ASP.Data;
 using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Get the connection string from configuration.
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContext") 
+    ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.");
+
+// Register ApplicationDbContext with SQLite.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+// Add Identity services.
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
 // Add services to the container.
+builder.Services.AddRazorPages();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddFluentUIComponents();
+
+
 
 var app = builder.Build();
 
@@ -22,6 +46,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapRazorPages();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
