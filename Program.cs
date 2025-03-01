f656683aa4cb0581e1126e642415904cbe9f1717
yagdali <yagdalir@gmail.com>
2025-02-28 20:05:12 -0500

    Fluent Blazor project transfer

--- Program.cs
+++ Program.cs
@@ -1,28 +1,53 @@
-using Microsoft.FluentUI.AspNetCore.Components;
 using ASP.Components;
+using ASP.Data;
+using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+var userDbConnectionString = builder.Configuration.GetConnectionString("ApplicationDbContext")
+    ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.");
+
+var plansDbConnectionString = builder.Configuration.GetConnectionString("PlansDbContext")
+    ?? throw new InvalidOperationException("Connection string 'PlansDbContext' not found.");
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(userDbConnectionString));
+
+
+builder.Services.AddDbContext<PlansDbContext>(options =>
+    options.UseSqlite(plansDbConnectionString));
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add services to the container
+builder.Services.AddRazorPages();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddFluentUIComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAntiforgery();
-
-app.MapStaticAssets();
+app.MapRazorPages();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
