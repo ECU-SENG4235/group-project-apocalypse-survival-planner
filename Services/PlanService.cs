534b738ec7e982c7c4d9e176b6de3841d4adaa62
Yagdali Rivas <rivasy21@students.ecu.edu>
2025-03-24 13:02:56 -0400

    The not broken update

--- Services/PlanService.cs
+++ Services/PlanService.cs
@@ -1,18 +1,59 @@
-using Microsoft.EntityFrameworkCore;
-using ASP.Data;
+﻿using ASP.Data;
 using ASP.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public class PlanService
 {
-    private readonly PlansDbContext _context;
+    private readonly PlansDbContext _dbContext;
 
-    public PlanService(PlansDbContext context)
+    public PlanService(PlansDbContext dbContext)
     {
-        _context = context;
+        _dbContext = dbContext;
     }
 
     public async Task<List<Plan>> GetPlansAsync()
     {
-        return await _context.Plans.ToListAsync();
+        return await _dbContext.Plans.ToListAsync();
+    }
+
+    public async Task<Plan> GetPlanByIdAsync(int id)
+    {
+        return await _dbContext.Plans.FindAsync(id);
+    }
+
+    public async Task AddPlanAsync(Plan plan)
+    {
+        _dbContext.Plans.Add(plan);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdatePlanAsync(Plan plan)
+    {
+        _dbContext.Entry(plan).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeletePlanAsync(int id)
+    {
+        var plan = await _dbContext.Plans.FindAsync(id);
+        if (plan != null)
+        {
+            _dbContext.Plans.Remove(plan);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+    public async Task SavePlanAsync(Plan plan)
+    {
+        if (plan.Id == 0)
+        {
+            await AddPlanAsync(plan);
+        }
+        else
+        {
+            await UpdatePlanAsync(plan);
+        }
     }
 }
+
