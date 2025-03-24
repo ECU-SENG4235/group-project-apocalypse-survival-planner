415188437d3a67e940ab1582f55a3f527802f347
Tvaughn22 <vaughnt22@students.ecu.edu>
2025-03-24 09:12:14 -0400

    added css for other identity pages

--- obj/Debug/net9.0/ASP.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASP.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2010414993c767aee5d63a1f2033036fe254b0ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45609ae02daac453f7035fe64b41f3865a97d681")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
