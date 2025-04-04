41cfbe98d62f01822aeff80291593ec66eaf568b
Yagdali Rivas <rivasy21@students.ecu.edu>
2025-04-04 12:24:28 -0400

    Survial time calulations, my plans, and prices added to create plan update

--- obj/Debug/net9.0/ASP.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASP.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fabcbdf7cccd834820abf2619509e289117ee5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f945c2d3ee67c5ac4b945087d5bb0eb34ee5fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
