70ed8396c30bc9ca3a7d4fc0bf64117ccb3c1da3
Nathan Panganiban <142838700+NathanPanganiban@users.noreply.github.com>
2025-04-16 16:02:30 -0400

    Added ItemCategory
    Contains methods for item number/price tracking

---
+++ Models/ItemCategory.cs
@@ -0,0 +1,41 @@
+﻿namespace ASP.Models
+{
+    public class ItemCategory
+{
+    public string[] Labels { get; set; }
+    public decimal[] Prices { get; set; }
+    public int[] Quantities { get; set; }
+
+    public List<string> GetSelected()
+    {
+        var list = new List<string>();
+        for (int i = 0; i < Quantities.Length; i++)
+        {
+            if (Quantities[i] > 0)
+                list.Add($"{Labels[i]} x{Quantities[i]}");
+        }
+        return list;
+    }
+
+    public decimal GetTotal()
+    {
+        decimal total = 0;
+        for (int i = 0; i < Quantities.Length; i++)
+        {
+            total += Quantities[i] * Prices[i];
+        }
+        return total;
+    }
+
+    public Dictionary<string, (int quantity, decimal price)> GetItemCounts()
+    {
+        var dict = new Dictionary<string, (int, decimal)>();
+        for (int i = 0; i < Quantities.Length; i++)
+        {
+            if (Quantities[i] > 0)
+                dict[Labels[i]] = (Quantities[i], Prices[i]);
+        }
+        return dict;
+    }
+}
+}
